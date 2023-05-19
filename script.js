3229d110a41b166d2701bada2791dd41b39c27bb
flabriola <127946400+flabriola@users.noreply.github.com>
2023-05-19 16:34:10 +0400

    media

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function showAboutSection(id) {
         }
     }
 }
+
+
+
+// header side nav functions 
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+  
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
